ca126bc783915a5b8414eb681abd9c1f618fdd6d
ramilmavlihanov <Ramil040201@gmail.com>
2023-10-30 03:52:19 +0300

    Стартовые цвета и шейпы

--- HnHApp/app/src/main/java/com/example/hnhapp/MainActivity.kt
+++ HnHApp/app/src/main/java/com/example/hnhapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.hnhapp
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.hnhapp.databinding.ActivityMainBinding
@@ -14,9 +17,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fitContentViewToInsets()
+
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
 
         navController = navHostFragment.navController
+    }
+
+    private fun fitContentViewToInsets(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+        } else {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
+        }
     }
 }
